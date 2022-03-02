edd410e7356381a460e9df9b44f78a34b09a013b
Joseph Neff <Jneff140@gmail.com>
2022-03-02 10:53:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Station=Base.classes.station
+measurement = Base.classes.measurement
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return(
+        f"Available Routes:<br/>"
+        f"Precipitaion: /api/v1.0/precipitation<br/>"
+        f"Stations: /api/v1.0/stations<br/>"
+        f"dates and temperature observations of the most active station for the last year of data: /api/v1.0/tobs<br/>"
+        f"Temperature from start date(format:yyyy-mm-dd): /api/v1.0/yyyy-mm-dd<br/>"
+        f"Temperature from start to end date(format:yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/>"
+        )
